2d9d8be3dd6c6d13255729f1354cf4ae1ab2906b
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-07-30 11:49:13 +0500

    change color of icon on hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Home from "./components/Home";
 import ScrollToTop from "react-scroll-to-top";
-import { IoIosArrowDropupCircle } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 export default function App() {
   return (
     <div className="App">
       <Home />
-      <ScrollToTop smooth component={<IoIosArrowDropupCircle size={26} />} style={{ background: "#163E7B", color: "white" }} />
+      <ScrollToTop smooth component={<IoIosArrowUp size={26} />} style={{ background: "#163E7B", color: "white" }} />
     </div>
   );
 }
