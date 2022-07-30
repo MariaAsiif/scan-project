f10b7eec9dd0b78f8cc387d8b131835816a59bfd
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-07-30 11:42:42 +0500

    add react-icon library and add custom icon in srollto to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from "./components/Home";
 import ScrollToTop from "react-scroll-to-top";
+import { IoIosArrowDropupCircle } from "react-icons/io";
 export default function App() {
   return (
     <div className="App">
       <Home />
-      <ScrollToTop smooth />
+      <ScrollToTop smooth component={<IoIosArrowDropupCircle size={26} />} style={{ background: "#163E7B", color: "white" }} />
     </div>
   );
 }
