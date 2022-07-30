f64917d65b36bb18ac4d7a6212d378127090fef0
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-07-30 11:31:00 +0500

    add scroll to top pkg add add scroll btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Home from "./components/Home";
-
+import ScrollToTop from "react-scroll-to-top";
 export default function App() {
   return (
     <div className="App">
       <Home />
+      <ScrollToTop smooth />
     </div>
   );
 }
